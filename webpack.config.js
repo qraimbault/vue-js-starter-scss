1302084ea9dd29ed0c94bcabd2fbe926029d3ef0
Quentin RAIMBAULT <quentin.raimbault@cloud-temple.com>
2018-09-27 15:12:37 +0200

    🔧 Auto load master.scss

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,21 @@ const config = {
             }
           }
         ]
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              // you can also read from a file, e.g. `variables.scss`
+              data: `@import "@/scss/master.scss";`
+            }
+          }
+        ]
+      },
     ]
   },
   plugins: [
